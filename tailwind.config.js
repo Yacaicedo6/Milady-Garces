b4bb0bb38af15d80e6d5cd01b74c971a57c7087b
Yan Caicedo <yacaicedo6@poligran.edu.co>
2025-10-23 21:16:37 -0500

    prueba2.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif']
-
-
-      }
-    }
+        sans: ['"Inter"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-};
+}
